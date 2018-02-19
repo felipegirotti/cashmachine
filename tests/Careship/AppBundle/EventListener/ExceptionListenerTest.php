c29ce25c765c83dc937598739088544aa47b5757
Felipe Girotti <felipe.girotti@gmail.com>
2018-02-19 21:38:06 +0100

    add test to cover ExceptionListener

---
+++ tests/Careship/AppBundle/EventListener/ExceptionListenerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\AppBundle\EventListener;
+
+use Careship\AppBundle\EventListener\ExceptionListener;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Tests\Event\GetResponseForExceptionEventTest;
+use Symfony\Component\HttpKernel\Tests\TestHttpKernel;
+
+class ExceptionListenerTest extends TestCase
+{
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     * @param int $httpCode
+     * @dataProvider onKernelExceptionDataProvider
+     */
+    public function testOnKernelException(GetResponseForExceptionEvent $event, int $httpCode)
+    {
+        $listener = new ExceptionListener();
+        $listener->onKernelException($event);
+
+        $this->assertEquals($httpCode, $event->getResponse()->getStatusCode());
+    }
+
+    public function onKernelExceptionDataProvider()
+    {
+        return [
+            [new GetResponseForExceptionEvent(new TestHttpKernel(), new Request(), 1, new \Exception('error')), 500],
+            [new GetResponseForExceptionEvent(new TestHttpKernel(), new Request(), 1, new \InvalidArgumentException('error')), 400],
+        ];
+    }
+}
